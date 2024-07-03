8fa41ff548fb145237d54466be83347b189036e4
mmounarath <mmounarath@aol.com>
2024-07-03 14:03:39 -0700

    Requirement #6

--- Calculator/DataHandler.cs
+++ Calculator/DataHandler.cs
@@ -8,6 +8,12 @@ public class DataHandler
 {
     int[] _intArrayValues = null;
     string _strErrorMessage = null;
+    char _chrCustomDelimeter = '\0';
+
+    public char CustomDelimeter
+    {
+        get { return _chrCustomDelimeter; }
+    }
 
     public int[] ValueArray
     {
@@ -24,14 +30,36 @@ public class DataHandler
 
     }
 
+    public bool CheckCustomDelimeter(string strValue)
+    {
+        bool blnDelimeterFound = false;
+
+        if ((strValue.Length == 3)
+            && strValue[0] == '/'
+            && strValue[1] == '/')
+        {
+            blnDelimeterFound = true;
+            _chrCustomDelimeter = strValue[2];
+        }
+
+        return blnDelimeterFound;
+    }
+
     public void ParseData(string strCSV)
     {
         bool blnIsNumeric;
         int intTempValue;
-        string[] strArrayDelimiters = { ",", "\\n" };
-
+        string[] strArrayDelimiters = new string[2] { ",", "\\n" };
         string[] strArrayValues = strCSV.Split(strArrayDelimiters, StringSplitOptions.None);
+        
+        if (CheckCustomDelimeter(strArrayValues[0]) == true)
+        {
+            strArrayDelimiters = new string[3] { ",", "\\n", _chrCustomDelimeter.ToString() };
+            strCSV = strCSV.Remove(0, 5);
+            strArrayValues = strCSV.Split(strArrayDelimiters, StringSplitOptions.None);
+        }
         _intArrayValues = new int[strArrayValues.Length];
+ 
         for (int i = 0; i < strArrayValues.Length; i++)
         {
             _intArrayValues[i] = 0;
--- TestCalculator/TestDataHandler.cs
+++ TestCalculator/TestDataHandler.cs
@@ -22,6 +22,14 @@ namespace TestCalculator
         }
 
         [TestMethod]
+        public void CheckCustomDelimter()
+        {
+            DataHandler handler = new DataHandler();
+            handler.CheckCustomDelimeter("//#");
+            Assert.AreEqual(handler.CustomDelimeter, '#');
+        }
+
+        [TestMethod]
         public void ParseData()
         {
             DataHandler handler = new DataHandler();
@@ -30,6 +38,17 @@ namespace TestCalculator
             Assert.AreEqual(handler.ValueArray[1], 2);
             Assert.AreEqual(handler.ValueArray[2], 3);
         }
+
+        [TestMethod]
+        public void ParseData_WithCustomDelimeter()
+        {
+            DataHandler handler = new DataHandler();
+            handler.ParseData("//#\\n2#5");
+            Assert.AreEqual(handler.CustomDelimeter, '#');
+            Assert.AreEqual(handler.ValueArray[0], 2);
+            Assert.AreEqual(handler.ValueArray[1], 5);
+        }
+
 
         [TestMethod]
         public void ParseData_WithCharacters()
